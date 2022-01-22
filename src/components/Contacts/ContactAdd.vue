aa2284ef80f46ead365cf3299c428ef990fe4e73
Rommel Canoy <rommelcanoy4@gmail.com>
2022-01-23 00:52:56 +0800

    add: add contact form

--- src/components/Contacts/ContactAdd.vue
+++ src/components/Contacts/ContactAdd.vue
@@ -1,7 +1,168 @@
 <template>
-  <div class="bg-gray-50 p-5 h-full">
-    <h2>Contact Add</h2>
-    
+  <div class="bg-gray-50 p-5 h-full flex justify-center items-center">
+    <div class="block p-6 rounded-lg shadow-lg bg-white w-full md:w-1/2 md:max-w-md">
+    <header class=" py-4 border-b border-gray-100">
+        <h2 class="font-semibold text-gray-800">Add Contact</h2>
+      </header>
+      <form>
+        <div class="form-group my-6">
+          <label
+            for="name"
+            class="form-label inline-block mb-2 text-gray-700"
+            >Name</label
+          >
+          <input
+            type="text"
+            class="
+              form-control
+              block
+              w-full
+              px-3
+              py-1.5
+              text-base
+              font-normal
+              text-gray-700
+              bg-white bg-clip-padding
+              border border-solid border-gray-300
+              rounded
+              transition
+              ease-in-out
+              m-0
+              focus:text-gray-700
+              focus:bg-white
+              focus:border-blue-600
+              focus:outline-none
+            "
+            id="name"
+            placeholder="Name"
+          />
+        </div>
+        
+        <div class="form-group mb-6">
+          <label
+            for="email"
+            class="form-label inline-block mb-2 text-gray-700"
+            >Email address</label
+          >
+          <input
+            type="email"
+            class="
+              form-control
+              block
+              w-full
+              px-3
+              py-1.5
+              text-base
+              font-normal
+              text-gray-700
+              bg-white bg-clip-padding
+              border border-solid border-gray-300
+              rounded
+              transition
+              ease-in-out
+              m-0
+              focus:text-gray-700
+              focus:bg-white
+              focus:border-blue-600
+              focus:outline-none
+            "
+            id="email"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+        </div>
+        <div class="form-group mb-6">
+          <label
+            for="exampleInputPassword2"
+            class="form-label inline-block mb-2 text-gray-700"
+            >Designation</label
+          >
+          <input
+            type="text"
+            class="
+              form-control
+              block
+              w-full
+              px-3
+              py-1.5
+              text-base
+              font-normal
+              text-gray-700
+              bg-white bg-clip-padding
+              border border-solid border-gray-300
+              rounded
+              transition
+              ease-in-out
+              m-0
+              focus:text-gray-700
+              focus:bg-white
+              focus:border-blue-600
+              focus:outline-none
+            "
+            id="exampleInputPassword2"
+            placeholder="Password"
+          />
+        </div>
+        <div class="form-group mb-6">
+          <label
+            for="exampleInputPassword2"
+            class="form-label inline-block mb-2 text-gray-700"
+            >Contact No.</label
+          >
+          <input
+            type="number"
+            class="
+              form-control
+              block
+              w-full
+              px-3
+              py-1.5
+              text-base
+              font-normal
+              text-gray-700
+              bg-white bg-clip-padding
+              border border-solid border-gray-300
+              rounded
+              transition
+              ease-in-out
+              m-0
+              focus:text-gray-700
+              focus:bg-white
+              focus:border-blue-600
+              focus:outline-none
+            "
+            id="exampleInputPassword2"
+            placeholder="Password"
+          />
+        </div>
+        
+        <button
+          type="submit"
+          class="
+            w-full
+            px-6
+            py-2.5
+            bg-blue-600
+            text-white
+            font-medium
+            text-xs
+            leading-tight
+            uppercase
+            rounded
+            shadow-md
+            hover:bg-blue-700 hover:shadow-lg
+            focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+            active:bg-blue-800 active:shadow-lg
+            transition
+            duration-150
+            ease-in-out
+          "
+        >
+          Add Contact
+        </button>
+        
+      </form>
+    </div>
   </div>
 </template>
 
