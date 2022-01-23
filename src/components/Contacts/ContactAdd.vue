502a1c5a0ae1adf7db82178822d585cc8184b31a
Rommel Canoy <rommelcanoy4@gmail.com>
2022-01-23 19:59:40 +0800

    add: save contact functionality

--- src/components/Contacts/ContactAdd.vue
+++ src/components/Contacts/ContactAdd.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="bg-gray-50 p-5 h-full flex justify-center items-center">
-    <div class="block p-6 rounded-lg shadow-lg bg-white w-full md:w-1/2 md:max-w-md">
-    <header class=" py-4 border-b border-gray-100">
+    <div
+      class="
+        block
+        p-6
+        rounded-lg
+        shadow-lg
+        bg-white
+        w-full
+        md:w-1/2 md:max-w-md
+      "
+    >
+      <header class="py-4 border-b border-gray-100">
         <h2 class="font-semibold text-gray-800">Add Contact</h2>
       </header>
-      <form>
+      <div
+        class="bg-red-100 rounded-lg py-5 px-6 mb-4 text-base text-red-700 mb-3"
+        role="alert"
+        v-if="errors.length"
+      >
+        <ul>
+          <li v-for="(error, index) in errors" :key="index">{{ error }}</li>
+        </ul>
+      </div>
+      <form @submit.prevent="saveContact" noValidate>
         <div class="form-group my-6">
-          <label
-            for="name"
-            class="form-label inline-block mb-2 text-gray-700"
+          <label for="name" class="form-label inline-block mb-2 text-gray-700"
             >Name</label
           >
           <input
@@ -34,17 +51,17 @@
               focus:outline-none
             "
             id="name"
-            placeholder="Name"
+            v-model="contact.name"
+            placeholder="Enter name"
           />
         </div>
-        
+
         <div class="form-group mb-6">
-          <label
-            for="email"
-            class="form-label inline-block mb-2 text-gray-700"
+          <label for="email" class="form-label inline-block mb-2 text-gray-700"
             >Email address</label
           >
           <input
+            v-model="contact.email"
             type="email"
             class="
               form-control
@@ -78,6 +95,7 @@
             >Designation</label
           >
           <input
+            v-model="contact.designation"
             type="text"
             class="
               form-control
@@ -99,8 +117,8 @@
               focus:border-blue-600
               focus:outline-none
             "
-            id="exampleInputPassword2"
-            placeholder="Password"
+            id="designation"
+            placeholder="Enter designation"
           />
         </div>
         <div class="form-group mb-6">
@@ -110,6 +128,7 @@
             >Contact No.</label
           >
           <input
+            v-model="contact.contact_no"
             type="number"
             class="
               form-control
@@ -131,11 +150,11 @@
               focus:border-blue-600
               focus:outline-none
             "
-            id="exampleInputPassword2"
-            placeholder="Password"
+            id="contact_no"
+            placeholder="Enter contact no."
           />
         </div>
-        
+
         <button
           type="submit"
           class="
@@ -160,15 +179,76 @@
         >
           Add Contact
         </button>
-        
       </form>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "ContactAdd",
+  data() {
+    return {
+      contact: {},
+      // name: "",
+      // email: "",
+      // designation: "",
+      // contact_no: "",
+      errors: [],
+    };
+  },
+  methods: {
+    async saveContact() {
+      this.errors = [];
+      if (!this.contact.name) {
+        this.errors.push("Name is required");
+      }
+      if (!this.contact.email) {
+        this.errors.push("Email is required");
+      }
+      if (!this.contact.designation) {
+        this.errors.push("Designation is required");
+      }
+      if (!this.contact.contact_no) {
+        this.errors.push("Contact no. is required");
+      }
+
+      if (!this.errors.length) {
+        let formData = new FormData();
+        formData.append("name", this.contact.name);
+        formData.append("email", this.contact.email);
+        formData.append("designation", this.contact.designation);
+        formData.append("contact_no", this.contact.contact_no);
+
+        let url = "http://127.0.0.1:8000/api/save_contact";
+
+        await axios
+          .post(url, formData)
+          .then((response) => {
+            console.log(response);
+            if (response.status == 200) {
+              this.contact.name = "";
+              this.contact.email = "";
+              this.contact.designation = "";
+              this.contact.contact_no = "";
+              this.$toast.open({
+                message: response.data.message,
+                type: "success",
+                position: "top",
+                // all of other options may go here
+              });
+            }
+            // else {
+            //   console.log("There's something wrong with the request");
+            // }
+          })
+          .catch((error) => {
+            console.error(error.response);
+          });
+      }
+    },
+  },
 };
 </script>
 
