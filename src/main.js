6cfd7b66e767d7c0b1b9163dbc3742060ac08989
Rommel Canoy <rommelcanoy4@gmail.com>
2022-01-23 20:00:19 +0800

    add: toast notification library

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueToast from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-default.css';
+// import 'vue-toast-notification/dist/theme-sugar.css';
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(VueToast).use(store).use(router).mount('#app')
+
